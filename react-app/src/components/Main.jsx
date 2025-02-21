bab4d67788cc6489e6a55eab672cb03d57a840c3
uzzer-name <uzzer@example.com>
2025-02-21 16:19:28 +0200

    fix

--- react-app/src/components/Main.jsx
+++ react-app/src/components/Main.jsx
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Main.css';
-import myImage from './images/image.webp'; 
 
 const Main = () => {
-    const [showImage, setShowImage] = useState(false);
-    const [headerText, setHeaderText] = useState('React first steps');
-    const [buttonClicked, setButtonClicked] = useState(false);
-
-    const handleClick = () => {
-      setShowImage(true);
-      setHeaderText('執行日期 明天凌晨 在天安門廣場'); 
-      setButtonClicked(true);
-    };
-
-    return (
-      <div className={`main-container ${buttonClicked ? 'red-border' : ''}`}>
-        <h1>{headerText}</h1>
-        {!showImage ? (
-          <p>I am alive</p>
-        ) : (
-          <img src={myImage} alt="My Description" />
-        )}
-        {!buttonClicked && (
-          <button className={`main-button`} onClick={handleClick}>
-            Click plz
-          </button>
-        )}
-      </div>
-    );
+  return (
+    <div className="main-container">
+      <h1>React first steps</h1>
+      <p className="text-container">My first app</p>
+        <button className="main-button">
+          Click
+        </button>
+    </div>
+  );
 };
 
 export default Main;
